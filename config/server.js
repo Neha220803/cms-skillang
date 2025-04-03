af6889e6b2d2000e18d96813cc466fd7182c9b14
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-04-03 17:09:32 +0530

    this

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  url: env("PUBLIC_URL", "https://skillang.com"),
-  proxy: true,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
