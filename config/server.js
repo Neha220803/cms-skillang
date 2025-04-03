1c0b996d7b6c4be5c725c8e37a91bc32061299c4
Neeharika Srini <neeharika.srinivasan@gmail.com>
2025-04-03 17:02:52 +0530

    minnor change

--- config/server.js
+++ config/server.js
@@ -1,10 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  server: {
+    https: env.bool("USE_HTTPS", false)
+      ? {
+          key: "/etc/letsencrypt/live/skillang.com/privkey.pem",
+          cert: "/etc/letsencrypt/live/skillang.com/fullchain.pem",
+        }
+      : undefined,
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
